04ff4b41faa531306b5e9a19bb953f838d974d35
borshon420 <borshonarafat420@gmail.com>
2021-10-07 02:38:47 +0600

    simple-firebase-authentication

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8H0nQmMcHT9_qnQzVuGI8koOBJDeOwwc",
+    authDomain: "simple-firebase-authenti-91b06.firebaseapp.com",
+    projectId: "simple-firebase-authenti-91b06",
+    storageBucket: "simple-firebase-authenti-91b06.appspot.com",
+    messagingSenderId: "886954454272",
+    appId: "1:886954454272:web:e0c75caef0451cd11557e5"
+  };
+
+  export default firebaseConfig;
